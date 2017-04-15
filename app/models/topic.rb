1002b8a7bc53d76e9b16c47dde4ea566f9d2acd4
Jordan Hudgens <jordan@wow.com>
2017-04-14 18:09:52 -0700

    Setup relationships between topics, users, and posts

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,3 +1,4 @@
 class Topic < ApplicationRecord
   belongs_to :user
+  has_many :posts
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :topics
+  has_many :posts
 end
