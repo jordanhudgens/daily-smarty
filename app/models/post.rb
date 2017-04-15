923983d3cd7de45f40d5be5f8ad3580aad6d96a2
Jordan Hudgens <jordan@wow.com>
2017-04-14 18:34:51 -0700

    Added slug call for friendly ID to topic and posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   belongs_to :topic
+
+  extend FriendlyId
+  friendly_id :title, use: :slugged
 end
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,4 +1,7 @@
 class Topic < ApplicationRecord
   belongs_to :user
   has_many :posts
+
+  extend FriendlyId
+  friendly_id :title, use: :slugged
 end
