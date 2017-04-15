6a5561174289306276948201b0d7dcfcf944b8d7
Jordan Hudgens <jordan@wow.com>
2017-04-14 18:08:56 -0700

    Added post scaffold to system

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170415010356) do
+ActiveRecord::Schema.define(version: 20170415010818) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,17 @@ ActiveRecord::Schema.define(version: 20170415010356) do
     t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type", using: :btree
     t.index ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id", using: :btree
     t.index ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type", using: :btree
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "content"
+    t.integer  "user_id"
+    t.integer  "topic_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["topic_id"], name: "index_posts_on_topic_id", using: :btree
+    t.index ["user_id"], name: "index_posts_on_user_id", using: :btree
   end
 
   create_table "topics", force: :cascade do |t|
@@ -64,5 +75,7 @@ ActiveRecord::Schema.define(version: 20170415010356) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "posts", "topics"
+  add_foreign_key "posts", "users"
   add_foreign_key "topics", "users"
 end
---
+++ spec/factories/posts.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :post do
+    title "MyString"
+    content "MyText"
+    user nil
+    topic nil
+  end
+end
