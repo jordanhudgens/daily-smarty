189518273cd3a62c8c4472400ce922cb8a252c9c
Jordan Hudgens <jordan@wow.com>
2017-04-17 19:21:51 -0700

    Addded initial tests to ensure factories are properly configured

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,8 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
+  before do
+    @post = FactoryGirl.build_stubbed(:post)
+  end
+
   it 'allows a post to be created' do
-    post = FactoryGirl.create(:post)
-    expect(post).to be_valid
+    expect(@post).to be_valid
+  end
+
+  it 'has an owning user' do
+    expect(@post.user.full_name).to eq('Jon Snow')
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'can create a user factory' do
+    user = FactoryGirl.create(:user)
+    expect(user).to be_valid
+  end
 end
