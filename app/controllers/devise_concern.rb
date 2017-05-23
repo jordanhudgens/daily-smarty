5a38796a2f939b1604e707f2c06589c142acc009
Jordan Hudgens <jordan@wow.com>
2017-05-23 15:35:39 -0700

    Fixed before action issue

--- app/controllers/devise_concern.rb
+++ app/controllers/devise_concern.rb
@@ -2,7 +2,7 @@ module DeviseConcern
   extend ActiveSupport::Concern
 
   included do
-    before_filter :configure_permitted_parameters, if: :devise_controller?
+    before_action :configure_permitted_parameters, if: :devise_controller?
   end
 
   def configure_permitted_parameters
