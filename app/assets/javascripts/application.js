2fd8b553a817762e855c596b2e59030090615f99
Jordan Hudgens <jordan@wow.com>
2017-06-06 10:14:24 -0700

    Implemented scripts for the has many through relationship between topics and posts

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,64 +18,18 @@
 //= require popper
 //= require bootstrap-sprockets
 
-$(document).ready(function() {
-    $("#owner a.add_fields").
-      data("association-insertion-position", 'before').
-      data("association-insertion-node", 'this');
-
-    $('#owner').bind('cocoon:after-insert',
-         function() {
-           $("#owner_from_list").hide();
-           $("#owner a.add_fields").hide();
-         });
-    $('#owner').bind("cocoon:after-remove",
-         function() {
-           $("#owner_from_list").show();
-           $("#owner a.add_fields").show();
-         });
-
-    $("#tags a.add_fields").
-      data("association-insertion-position", 'before').
-      data("association-insertion-node", 'this');
-
-    $('#tags').bind('cocoon:after-insert',
-         function(e, tag) {
-             console.log('inserting new tag ...');
-             $(".project-tag-fields a.add-tag").
-                 data("association-insertion-position", 'after').
-                 data("association-insertion-node", 'this');
-             $(this).find('.project-tag-fields').bind('cocoon:after-insert',
-                  function() {
-                    console.log('insert new tag ...');
-                    console.log($(this));
-                    $(this).find(".tag_from_list").remove();
-                    $(this).find("a.add_fields").hide();
-                  });
-         });
-
-    $('.project-tag-fields').bind('cocoon:after-insert',
-        function(e) {
-            console.log('replace OLD tag ...');
-            e.stopPropagation();
-            console.log($(this));
-            $(this).find(".tag_from_list").remove();
-            $(this).find("a.add_fields").hide();
-        });
-
-
-    $('#tasks').bind('cocoon:before-insert', function(e,task_to_be_added) {
-        console.log(task_to_be_added);
-        task_to_be_added.fadeIn('slow');
-    });
-
-    $('#tasks').bind('cocoon:after-insert', function(e, added_task) {
-        //added_task.css("background","red");
-    });
-
-    $('#tasks').bind('cocoon:before-remove', function(e, task) {
-        $(this).data('remove-timeout', 1000);
-        task.fadeOut('slow');
-    })
-
+function ready() {
     $(".dropdowns select").select2();
-});
+
+    $(document).on('click', '.add-topic', function(event) {
+      $(this).parent().find('.dropdowns select').select2();
+    });
+
+    $(document).on('click', '.add-custom-topic', function(event) {
+      $(this).hide();
+    });
+};
+
+$(document).ready(ready);
+$(document).on('turbolinks:load', ready);
+
