04ab6132aec9e7f47c759706f0672e0326a0703b
Jordan Hudgens <jordan@wow.com>
2017-06-07 19:09:48 -0700

    Integrated markdown parser for post show pages

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,22 @@
 module PostsHelper
+  class CodeRayify < Redcarpet::Render::HTML
+    def block_code(code, language)
+      language ||= :plaintext
+      CodeRay.scan(code, language).div
+    end
+  end
+
+  def markdown(text)
+    coderayified = CodeRayify.new(filter_html: true, hard_wrap: true)
+
+    options = {
+      fenced_code_blocks: true,
+      no_intra_emphasis: true,
+      autolink: true,
+      lax_html_blocks: true,
+    }
+
+    markdown_to_html = Redcarpet::Markdown.new(coderayified, options)
+    markdown_to_html.render(text).html_safe
+  end
 end
