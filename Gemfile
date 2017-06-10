2ff9d9cd916eae69c48d871b69b17c7291da17a5
Jordan Hudgens <jordan@wow.com>
2017-06-09 18:16:36 -0700

    Added styles for the homepage and pagination

--- Gemfile
+++ Gemfile
@@ -44,3 +44,4 @@ gem 'coderay', '~> 1.1', '>= 1.1.1'
 gem 'gritter', '~> 1.2'
 gem "font-awesome-rails"
 gem 'compass-rails'
+gem 'kaminari', '~> 1.0', '>= 1.0.1'
--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,6 +1,6 @@
 class StaticController < ApplicationController
   def homepage
     @page_title = 'DailySmarty | A Tool for Learning Something New Everyday'
-    @posts = Post.limit(20)
+    @posts = Post.page(params[:page]).per(5)
   end
 end
