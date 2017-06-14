5e87dd01f11fd2bceee5ace48eaca9e33ba3a3bb
Jordan Hudgens <jordan@wow.com>
2017-06-13 19:48:53 -0700

    Customized the styles on the homepage

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,6 +1,7 @@
 class StaticController < ApplicationController
   def homepage
     @page_title = 'DailySmarty | A Tool for Learning Something New Everyday'
-    @posts = Post.page(params[:page]).per(5)
+    #@posts = Post.page(params[:page]).per(5)
+    @topics = Topic.top_ten
   end
 end
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -6,4 +6,12 @@ class Topic < ApplicationRecord
   friendly_id :title, use: :slugged
 
   validates_presence_of :title
+
+  def self.top_ten
+    select('topics.*, COUNT(posts.id) AS posts_count').
+      joins(:posts).
+      group('topics.id').
+      order('posts_count DESC').
+      limit(10)
+  end
 end
