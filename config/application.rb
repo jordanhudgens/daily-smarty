20ca50ef8267e24e072f79849eb2cc1049d903cd
Jordan Hudgens <jordan@wow.com>
2017-06-17 07:58:37 -0700

    Updated initializer for asset compilation for ckeditor

--- config/application.rb
+++ config/application.rb
@@ -18,8 +18,8 @@ Bundler.require(*Rails.groups)
 
 module Dailysmarty
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.assets.precompile += Ckeditor.assets
+    config.assets.precompile += %w( ckeditor/* )
+    config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
   end
 end
