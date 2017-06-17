7768cc97f9521a13a1a4462cf8404129ca2324c1
Jordan Hudgens <jordan@wow.com>
2017-06-16 17:18:09 -0700

    Integrated a new user profile nav item and profile page that renders all of the user posts

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :posts
   resources :topics, only: [:index, :show]
   devise_for :users
-  get ':id', to: 'static#profile'
+  get ':id', to: 'static#profile', as: 'profile'
   root to: 'static#homepage'
 end
