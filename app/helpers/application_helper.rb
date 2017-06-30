ff801182ee8185f32ba409f0248444b9cfddd2d4
Jordan Hudgens <jordan@wow.com>
2017-06-30 09:40:57 -0700

    Integrated google analytics code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def google_analytics
+    tracking_code = <<~JS
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-101905610-1', 'auto');
+      ga('send', 'pageview');
+    JS
+
+    javascript_tag(tracking_code)
+  end
 end
