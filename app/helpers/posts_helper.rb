17d044da2ea7c871e34d4552daac70fd9408ce50
Jordan Hudgens <jordan@wow.com>
2017-07-21 12:53:16 -0700

    Built out the helper modules for styling post links

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -4,7 +4,18 @@ module PostsHelper
       link = LinkThumbnailer.generate(post_link)
       post_link_mapper(link)
     rescue LinkThumbnailer::Exceptions => e
-      return content_tag(:div, 'There was an error loading this link')
+      content = <<~EOL
+                  <div class='card'>
+                    <div class='row'>
+                      <div class='col-md-12'>
+                        <h2>
+                          <a href='#{post_link}'>#{post_link}>
+                        </h2>
+                      </div>
+                    </div>
+                  </div>
+                EOL
+      content_tag(:div, content.html_safe, class: 'post-link-wrapper')
     end
   end
 
@@ -19,24 +30,56 @@ module PostsHelper
   end
 
   def format_title_link(link)
-    content_tag :div, class: 'card' do
-      content_tag(:h2, link.title)
-      content_tag(:p, link.description)
-    end
+    content = <<~EOL
+                <div class='card'>
+                  <div class='row'>
+                    <div class='col-md-12'>
+                      <h2>
+                        <a href='#{link.url}' target='_blank'>#{link.title}</a>
+                      </h2>
+                      <p>#{link.description}</p>
+                    </div>
+                  </div>
+                </div>
+              EOL
+    content_tag(:div, content.html_safe, class: 'post-link-wrapper')
   end
 
   def format_video_link(link)
     content = <<~EOL
-                <h2>#{link.title}</h2>
-                <p>#{link.description}</p>
+                <div class='card'>
+                  <div class='row'>
+                    <div class='col-md-4'>
+                      <div>#{link.videos.first.embed_code}</div>
+                    </div>
+                    <div class='col-md-8'>
+                      <h2>
+                        <a href='#{link.url}' target='_blank'>#{link.title}</a>
+                      </h2>
+                      <p>#{link.description}</p>
+                    </div>
+                  </div>
+                </div>
               EOL
-    content_tag(:div, content.html_safe, class: 'card')
+    content_tag(:div, content.html_safe, class: 'post-link-wrapper')
   end
 
   def format_image_link(link)
-    content_tag :div, class: 'card' do
-      content_tag(:h2, link.title)
-      content_tag(:p, link.description)
-    end
+    content = <<~EOL
+                <div class='card'>
+                  <div class='row'>
+                    <div class='col-md-4'>
+                      <img src='#{link.images.first.src.to_s}'>
+                    </div>
+                    <div class='col-md-8'>
+                      <h2>
+                        <a href='#{link.url}' target='_blank'>#{link.title}</a>
+                      </h2>
+                      <p>#{link.description}</p>
+                    </div>
+                  </div>
+                </div>
+              EOL
+    content_tag(:div, content.html_safe, class: 'post-link-wrapper')
   end
 end
