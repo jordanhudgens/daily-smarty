635de5259c0a29462e49e48202fbbd67d2401884
Jordan Hudgens <jordan@wow.com>
2017-07-21 10:14:23 -0700

    Fixed the faraday bug by adding the lib to the applicationrb file

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ require "action_mailer/railtie"
 require "action_view/railtie"
 require "action_cable/engine"
 require "sprockets/railtie"
+require 'typhoeus/adapters/faraday'
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
