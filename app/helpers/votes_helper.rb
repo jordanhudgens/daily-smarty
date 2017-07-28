e7acb508fcae4ae076cef937292d18b96002fd5e
Jordan Hudgens <jordan@wow.com>
2017-07-27 22:10:17 -0700

    Updated the upvote/downvote functionality to be based on the user votes

--- app/helpers/votes_helper.rb
+++ app/helpers/votes_helper.rb
@@ -2,11 +2,11 @@ module VotesHelper
   def arrow_color(post, direction)
     if !current_user
       'novote'
-    elsif Vote.find_by_post_id(post.id).nil?
+    elsif current_user.votes.find_by_post_id(post.id).nil?
       'novote'
-    elsif Vote.find_by_post_id(post.id).down? && direction == 'down'
+    elsif current_user.votes.find_by_post_id(post.id).down? && direction == 'down'
       'downvote'
-    elsif Vote.find_by_post_id(post.id).up? && direction == 'up'
+    elsif current_user.votes.find_by_post_id(post.id).up? && direction == 'up'
       'upvote'
     else
       'novote'
