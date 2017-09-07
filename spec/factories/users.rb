cbfa7ab04b26372712354df3fec6d9cf20b4f532
Jordan Hudgens <jordan@wow.com>
2017-09-06 17:14:58 -0700

    Fixed failing test for users by updating the factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,10 +3,15 @@ FactoryGirl.define do
     "test#{n}@example.com"
   end
 
+	sequence :username do |n|
+    "jonsnow#{n}"
+  end
+
   factory :user do
     email { generate :email }
     password 'asdfasdf'
     password_confirmation 'asdfasdf'
     full_name 'Jon Snow'
+    username { generate :username }
   end
 end
