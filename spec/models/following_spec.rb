98481a0ac9f4c8cea124514776bf45ef77548679
Jordan Hudgens <jordan@wow.com>
2017-09-11 16:16:20 -0700

    Built out the specs for testing self referential user queries

--- spec/models/following_spec.rb
+++ spec/models/following_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Following, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'allows a user to follow another user and to query the followers' do
+    user_one = FactoryGirl.create(:user)
+    user_two = FactoryGirl.create(:user)
+
+    Following.create!(follower_id: user_one.id, followed_id: user_two.id)
+
+    expect(user_two.followers.pluck(:id).include?(user_one.id)).to eq(true)
+  end
+
+  it 'allows a user to follow another user and to query the list of users they are following' do
+    user_one = FactoryGirl.create(:user)
+    user_two = FactoryGirl.create(:user)
+
+    Following.create!(follower_id: user_one.id, followed_id: user_two.id)
+
+    expect(user_one.following.pluck(:id).include?(user_two.id)).to eq(true)
+  end
 end
