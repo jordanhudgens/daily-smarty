48960bafbfbfb2657596c7aa7d79e17d3b2df34b
Jordan Hudgens <jordan@wow.com>
2017-09-12 13:56:56 -0700

    Built out the callbacks and validations for the api client class

--- app/models/api_client.rb
+++ app/models/api_client.rb
@@ -1,3 +1,14 @@
 class ApiClient < ApplicationRecord
   belongs_to :user
+
+  validates_presence_of :source_app, :api_key
+  validates_uniqueness_of :source_app, :api_key
+
+  before_validation :set_api_key
+
+  private
+
+    def set_api_key
+      self.api_key = ApiKey.generator
+    end
 end
