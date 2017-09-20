e53b1778c85127a70a64fadf741076d8b1e127c8
Jordan Hudgens <jordan@wow.com>
2017-09-20 07:29:50 -0700

    Created topic following specs

--- spec/models/topic_following_spec.rb
+++ spec/models/topic_following_spec.rb
@@ -1,18 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe TopicFollowing, type: :model do
+  before do
+    @user = FactoryGirl.create(:user)
+  end
+
   it 'allows a user to follow a topic and to query the followers' do
-    user  = FactoryGirl.create(:user)
     topic = FactoryGirl.create(:topic)
 
-    TopicFollowing.create(topic_id: topic.id, user_id: user.id)
+    create(:topic_following, topic: topic, user: @user)
 
-    expect(topic.topic_followings.pluck(:id).include?(user.id)).to eq(true)
+    expect(topic.users.pluck(:id)).to eq([@user.id])
   end
 
   it 'allows a user to follow multiple topics and to query the posts from the topics they are following' do
-    user = FactoryGirl.create(:user)
-
     topic_one = FactoryGirl.create(:topic)
     post_one  = FactoryGirl.create(:post)
     Theme.create(topic: topic_one, post: post_one)
@@ -21,9 +22,9 @@ RSpec.describe TopicFollowing, type: :model do
     post_two  = FactoryGirl.create(:post_two)
     Theme.create(topic: topic_two, post: post_two)
 
-    TopicFollowing.create(topic_id: topic_one.id, user_id: user.id)
-    TopicFollowing.create(topic_id: topic_two.id, user_id: user.id)
+    create(:topic_following, topic: topic_one, user: @user)
+    create(:topic_following, topic: topic_two, user: @user)
 
-    expect(user.topics.pluck(:id)).to eq([topic_one.id, topic_two.id])
+    expect(@user.topics.pluck(:id)).to eq([topic_one.id, topic_two.id])
   end
 end
