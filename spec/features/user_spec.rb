d33c528bc8cf609c51dc432a5bde8a795f8785b3
Jordan Hudgens <jordan@wow.com>
2017-09-20 07:29:35 -0700

    Built out integration test for user registration page

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -1,9 +1,15 @@
 require 'rails_helper'
 
 describe 'User feature' do
-  xit 'should allow users to register on a custom login page' do
+  it 'should allow users to register' do
     visit new_user_registration_path
 
+    fill_in 'user[full_name]', with: 'Jon Snow'
+    fill_in 'user[username]', with: 'kingofthenorth'
+    fill_in 'user[email]', with: 'jon@snow.com'
+    fill_in 'user[password]', with: 'asdfasdf'
+    fill_in 'user[password_confirmation]', with: 'asdfasdf'
 
+    expect { click_on "Sign up" }.to change(User, :count).by(1)
   end
 end
