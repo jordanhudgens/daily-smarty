c887d2e89d39b9ef4bf50324d72bf8524a5b17d9
Jordan Hudgens <jordan@wow.com>
2017-09-21 13:44:26 -0700

    Added spec for ensuring that a user can sign in
    Additionally it ensures:
    
    - Shows feed on homepage

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -14,4 +14,17 @@ describe 'navigation' do
     expect(page).to_not have_link('Login')
     expect(page).to_not have_link('Register')
   end
+
+  it 'it allows the user to login and redirects them to the homepage' do
+    user = FactoryGirl.create(:user)
+
+    visit new_user_session_path
+
+    fill_in 'user[email]', with: user.email
+    fill_in 'user[password]', with: 'asdfasdf'
+
+    click_on 'Login'
+
+    expect(page).to have_content('My Feed')
+  end
 end
