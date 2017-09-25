8ff7d95638a362e3e4033f69ba856f6fac76a8c7
Jordan Hudgens <jordan@wow.com>
2017-09-25 10:58:17 -0700

    Added initial request specs for the post API system

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,9 +2,24 @@ require 'rails_helper'
 
 RSpec.describe "Posts", type: :request do
   describe "GET /posts" do
-    it "works! (now write some real specs)" do
-      get posts_path
-      expect(response).to have_http_status(200)
+    it "can request the posts API via a GET request and does not require auth" do
+      get posts_url(subdomain: 'api')
+      expect(response.content_type).to eq("application/json")
+      expect(response).to have_http_status(:ok)
+    end
+
+    xit "retrieves a list of posts" do
+      post_one = FactoryGirl.create(:post)
+      post_two = FactoryGirl.create(:post)
+      get posts_url(subdomain: 'api')
+      expect(response.content_type).to eq("application/json")
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe "POST /posts" do
+    xit 'can create a post with the proper auth' do
+
     end
   end
 end
