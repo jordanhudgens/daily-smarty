834ff10999098b8278c22d92f2b0d6d21ce8667b
jordanhudgens <jordan@devcamp.com>
2019-02-25 23:54:54 -0700

    Added style fixes and updated to include ssl requirement

--- config/application.rb
+++ config/application.rb
@@ -28,6 +28,9 @@ module Dailysmarty
     config.autoload_paths << Rails.root.join('lib')
     config.exceptions_app = self.routes
 
+    config.force_ssl = true
+    config.ssl_options = {  redirect: { status: 307, port: 81 } }
+
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
