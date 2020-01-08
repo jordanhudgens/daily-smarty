e6143f0f64cf67f297879b752aee67b576797ccd
jordanhudgens <jordan@devcamp.com>
2020-01-07 17:49:12 -0700

    Re-configured the order system for topics

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -21,6 +21,9 @@ class Topic < ApplicationRecord
   end
 
   def self.order_by_post_count
-    order('themes_count DESC')
+    select('topics.*, COUNT(posts.id) AS posts_count').
+      joins(:posts).
+      group('topics.id').
+      order('posts_count DESC')
   end
 end
