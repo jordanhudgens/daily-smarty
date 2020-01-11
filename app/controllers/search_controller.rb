b678e190c710ac0ed2ae2229de706eb65533591a
jordanhudgens <jordan@devcamp.com>
2020-01-11 13:26:03 -0700

    Added an extended conditional for cases when the search page is hit without params

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,13 +1,15 @@
 class SearchController < ApplicationController
   def query
-    if params[:q].blank?
-      redirect_to request.referer, notice: 'You need to enter a search query'
-    else
+    if !params[:q].blank?
       @posts = Post
                  .published
                  .search_by_term(params[:q])
                  .page(params[:page])
                  .per(10)
+    elsif params[:q].blank? && request
+      redirect_to request.referer, notice: 'You need to enter a search query'
+    else
+      redirect_to root_path, notice: 'You need to enter a search query'
     end
   end
 end
